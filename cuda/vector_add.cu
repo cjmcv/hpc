d091b1728e4450091f338fa88566a7fe991a190e
Jianming Chen <398342899@qq.com>
2018-01-06 22:55:10 +0800

    [cuda] Add CUDA_CHECK to vector_add

--- cuda/vector_add.cu
+++ cuda/vector_add.cu
@@ -6,6 +6,16 @@
 #include <cuda_runtime.h>
 #include "device_launch_parameters.h"
 
+#define CUDA_CHECK(condition) \
+  do { \
+    cudaError_t error = condition; \
+    if (error != cudaSuccess) { \
+      fprintf(stderr, "CUDA_CHECK error in line %d of file %s \
+              : %s \n", __LINE__, __FILE__, cudaGetErrorString(cudaGetLastError()) ); \
+      exit(EXIT_FAILURE); \
+    } \
+  } while(0);
+
 // Kernel
 __global__ void VectorAddKernel(const float *A, const float *B,
                                 float *C, int num) {
@@ -18,51 +28,31 @@ __global__ void VectorAddKernel(const float *A, const float *B,
 int VectorAdd(const float *h_a, const float *h_b, const int num, float *h_c) {
   size_t size = num * sizeof(float);
   // Allocate the device input vector
-  cudaError_t cuda_err;
-  cudaError_t cuda_err_a, cuda_err_b, cuda_err_c;
   float *d_a = NULL;
   float *d_b = NULL;
   float *d_c = NULL;
 
-  cuda_err_a = cudaMalloc((void **)&d_a, size);
-  cuda_err_b = cudaMalloc((void **)&d_b, size);
-  cuda_err_c = cudaMalloc((void **)&d_c, size);
-  if (cuda_err_a != cudaSuccess || cuda_err_b != cudaSuccess || cuda_err_c != cudaSuccess) {
-    fprintf(stderr, "Failed to allocate device vector (error code %s)!\n");
-    return 1;
-  }
+  CUDA_CHECK(cudaMalloc((void **)&d_a, size));
+  CUDA_CHECK(cudaMalloc((void **)&d_b, size));
+  CUDA_CHECK(cudaMalloc((void **)&d_c, size));
 
   // Copy the host input vectors in host memory to the device input vectors in
   // device memory
   printf("Copy input data from the host memory to the CUDA device\n");
-  cuda_err_a = cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
-  cuda_err_b = cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
-  if (cuda_err_a != cudaSuccess || cuda_err_a != cudaSuccess) {
-    fprintf(stderr, "Failed to copy vector from host to device (error code <%s>, <%s>)!\n", 
-      cudaGetErrorString(cuda_err_a), cudaGetErrorString(cuda_err_b));
-    return 1;
-  }
+  CUDA_CHECK(cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice));
 
   // Launch the Vector Add CUDA Kernel
   int threadsPerBlock = 256;
   int blocksPerGrid = (num + threadsPerBlock - 1) / threadsPerBlock;
   printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
   VectorAddKernel << <blocksPerGrid, threadsPerBlock >> > (d_a, d_b, d_c, num);
-  cuda_err = cudaGetLastError();
-  if (cuda_err != cudaSuccess) {
-    fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(cuda_err));
-    return 1;
-  }
+  CUDA_CHECK(cudaGetLastError());
 
   // Copy the device result vector in device memory to the host result vector
   // in host memory.
   printf("Copy output data from the CUDA device to the host memory\n");
-  cuda_err_c = cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
-
-  if (cuda_err_c != cudaSuccess) {
-    fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(cuda_err_c));
-    return 1;
-  }
+  CUDA_CHECK(cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost));
 
   // Verify that the result vector is correct
   for (int i = 0; i < num; ++i) {
@@ -73,16 +63,9 @@ int VectorAdd(const float *h_a, const float *h_b, const int num, float *h_c) {
   }
 
   // Free device global memory
-  cuda_err_a = cudaFree(d_a);
-  cuda_err_b = cudaFree(d_b);
-  cuda_err_c = cudaFree(d_c);
-  if (cuda_err_a != cudaSuccess || cuda_err_b != cudaSuccess || cuda_err_c != cudaSuccess) {
-    fprintf(stderr, "Failed to free device vector (error code <%s>, <%s>, <%s>)!\n", 
-      cudaGetErrorString(cuda_err_a), 
-      cudaGetErrorString(cuda_err_b),
-      cudaGetErrorString(cuda_err_c));
-    return 1;
-  }
+  CUDA_CHECK(cudaFree(d_a));
+  CUDA_CHECK(cudaFree(d_b));
+  CUDA_CHECK(cudaFree(d_c));
 
   // Reset the device and exit
   // cudaDeviceReset causes the driver to clean up all state. While
@@ -90,11 +73,7 @@ int VectorAdd(const float *h_a, const float *h_b, const int num, float *h_c) {
   // needed to ensure correct operation when the application is being
   // profiled. Calling cudaDeviceReset causes all profile data to be
   // flushed before the application exits
-  cuda_err = cudaDeviceReset();
-  if (cuda_err != cudaSuccess) {
-    fprintf(stderr, "Failed to deinitialize the device! error=%s\n", cudaGetErrorString(cuda_err));
-    return 1;
-  }
+  CUDA_CHECK(cudaDeviceReset());
 
   printf("Done\n");
   return 0;
