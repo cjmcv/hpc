8b4d58be19d2bb35a2c395170765ac094bd459d7
Jianming Chen <398342899@qq.com>
2018-01-08 22:56:01 +0800

    [sse] Update gemm

--- sse/matrix_multiply.cpp
+++ sse/matrix_multiply.cpp
@@ -7,43 +7,48 @@
 #include "xmmintrin.h"
 
 // Initialize the input data.
-void GenMatrix(float *mat, int width) {
-  for (int i = 0; i < width; i++) {
+void GenMatrix(const int height, const int width, float *mat) {
+  for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
       mat[i*width + j] = (float)rand() / RAND_MAX + (float)rand() / (RAND_MAX*RAND_MAX);
     }
   }
 }
 
-void MatrixMulNormal(const float* matA, const float* matB, float* matRet, int width) {
-  for (int i = 0; i < width; i++) {
-    for (int j = 0; j < width; j++) {
-      float sum = 0;
-      for (int k = 0; k < width; k++) {
-        sum += matA[i * width + k] * matB[k * width + j];
+void MatrixMulNormal(const int M, const int N, const int K, const float ALPHA,
+  const float *A, const int lda,
+  const float *B, const int ldb,
+  float *C, const int ldc) {
+  int i, j, k;
+  memset(C, 0, sizeof(float) * ldc * M);
+  for (i = 0; i < M; ++i) {
+    for (k = 0; k < K; ++k) {
+      register float A_PART = ALPHA*A[i*lda + k];
+      for (j = 0; j < N; ++j) {
+        C[i*ldc + j] += A_PART*B[k*ldb + j];
       }
-      matRet[i * width + j] = sum;
     }
   }
 }
 
-void MatrixMulSSE(const float* matA, const float* matB, float* matRet, int width) {
-  if (width < 8)
-    return;
-
-  for (int i = 0; i < width; i++) {
-    for (int j = 0; j < width; j++) {
-      __m128 sum = _mm_setzero_ps();
-      for (int k = 0; k < width; k += 8) {
-        __m128 a0 = _mm_loadu_ps(matA + i*width + k);
-        __m128 a1 = _mm_loadu_ps(matA + i*width + k + 4);
-
-        // Notice that the order of _mm_set_ps is reversed
-        __m128 b0 = _mm_set_ps(matB[(k + 3)*width + j], matB[(k + 2)*width + j], matB[(k + 1)*width + j], matB[k*width + j]);
-        __m128 b1 = _mm_set_ps(matB[(k + 7)*width + j], matB[(k + 6)*width + j], matB[(k + 5)*width + j], matB[(k + 4)*width + j]);
-        sum = _mm_add_ps(sum, _mm_add_ps(_mm_mul_ps(a0, b0), _mm_mul_ps(a1, b1)));
+void MatrixMulSSE(const int M, const int N, const int K, const float ALPHA,
+  const float *A, const int lda,
+  const float *B, const int ldb,
+  float *C, const int ldc) {  
+  int i, j, k;
+  memset(C, 0, sizeof(float) * ldc * M);
+  for (i = 0; i < M; ++i) {
+    for (k = 0; k < K; ++k) {
+      __m128 apart = _mm_set_ps1(ALPHA*A[i*lda + k]);
+      for (j = 0; j < N - 3; j += 4) {
+        __m128 b = _mm_loadu_ps(B + k*ldc + j);
+        __m128 c = _mm_loadu_ps(C + i*ldc + j);
+        c = _mm_add_ps(c, _mm_mul_ps(apart, b));
+        _mm_storeu_ps(C + i*ldc + j, c);
       }
-      matRet[i*width + j] = sum.m128_f32[0] + sum.m128_f32[1] + sum.m128_f32[2] + sum.m128_f32[3];
+      for (;j < N; j++) {
+        C[i*ldc + j] += apart.m128_f32[0] * B[k*ldb + j];
+      }
     }
   }
 }
@@ -70,44 +75,46 @@ void MatrixPrint(const float* mat, const int height, const int width) {
 }
 
 int main() {
-  int width = 128;
-  float *matA = new float[width * width];
-  float *matB = new float[width * width];
-  float *matRetOrg = new float[width * width];
-  float *matRetSSE = new float[width * width];
+  int height_a = 500, width_a = 40;
+  int height_b = 40, width_b = 1200;
+  if (width_a != height_b) {
+    std::cout << "input params are invalid." << std::endl;
+    return 1;
+  }
+  int height_ret = height_a, width_ret = width_b;
+
+  float *mat_a = new float[height_a * width_a];
+  float *mat_b = new float[height_b * width_b];
+  float *mat_ret_normal = new float[height_ret * width_ret];
+  float *mat_ret_sse = new float[height_ret * width_ret];
 
   srand(0);
-  GenMatrix(matA, width);
-  GenMatrix(matB, width);
+  GenMatrix(height_a, width_a, mat_a);
+  GenMatrix(height_b, width_b, mat_b);
 
   time_t stime;
+  stime = clock();
+  for (int i = 0; i < 100; i++) {
+    MatrixMulNormal(height_ret, width_ret, width_a, 1.0, mat_a, width_a, mat_b, width_b, mat_ret_normal, width_ret);
+  }
+  std::cout << "Normal ->  time: " << clock() - stime << ", mean value: " << GetMean(mat_ret_normal, height_ret, width_ret) << std::endl;
 
   stime = clock();
   for (int i = 0; i < 100; i++) {
-    MatrixMulNormal(matA, matB, matRetOrg, width);
+    MatrixMulSSE(height_ret, width_ret, width_a, 1.0, mat_a, width_a, mat_b, width_b, mat_ret_sse, width_ret);
   }
-  std::cout << "Normal ->  time: " << clock() - stime << std::endl;
-
-  stime = clock();
-  for (int i = 0; i < 100; i++) {
-    MatrixMulSSE(matA, matB, matRetSSE, width);
-  }
-  std::cout << "SSE ->  time: " << clock() - stime << std::endl;
-
-  printf("avg = %f \n", GetMean(matRetOrg, width, width));
-  printf("avg2 = %f \n", GetMean(matRetSSE, width, width));
+  std::cout << "SSE ->  time: " << clock() - stime << ", mean value: " << GetMean(mat_ret_sse, height_ret, width_ret) << std::endl;
 
   //std::cout << "\n Org result" << std::endl;
-  //MatrixPrint(matRetOrg, width, width);
+  //MatrixPrint(mat_ret_normal, height_ret, width_ret);
 
   //std::cout << "\n SSE result \n" << std::endl;
-  //MatrixPrint(matRetSSE, width, width);
+  //MatrixPrint(mat_ret_sse, height_ret, width_ret);
 
-  delete[] matA;
-  delete[] matB;
-  delete[] matRetOrg;
-  delete[] matRetSSE;
+  delete[] mat_a;
+  delete[] mat_b;
+  delete[] mat_ret_normal;
+  delete[] mat_ret_sse;
 
-  system("pause");
   return 0;
 }
