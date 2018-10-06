8f577f15f0107386985261ba2e9d64cc946ae757
Jianming Chen <398342899@qq.com>
2018-10-06 21:46:18 +0800

    [cuda] Update cub_warp_reduce.

--- cuda/cub_warp_reduce.cu
+++ cuda/cub_warp_reduce.cu
@@ -28,17 +28,56 @@ void SolveInCPU(T *h_in, T &h_out, int num_items) {
   }
 }
 
+enum WarpReduceMode {
+  FULL,
+  PARTIAL
+};
+
+/*
+*   The difference between Full and Partial is the input parameters
+* of cub::WarpReduce::Reduce.
+*
+*   In the mode Full, you can call the Reduce function like this :
+* T output = WarpReduce(temp_storage[warp_id]).Reduce(input, reduction_op);
+* And in the mode PARTIAL, then :
+* T output = WarpReduce(temp_storage[warp_id]).Reduce(input, reduction_op, valid_warp_threads);
+*
+*   PS: The Reduce functions in FULL and PARTIAL are implemented differently.
+*/
+// Full-tile warp reduction.
+template <typename T, typename ReductionOp, typename WarpReduce>
+static __device__ __forceinline__
+  T DeviceReduce(typename WarpReduce::TempStorage &temp_storage,
+                 T &data, 
+                 ReductionOp &reduction_op,
+                 cub::Int2Type<FULL> test_mode,
+                 const int &valid_warp_threads) {
+  return WarpReduce(temp_storage).Reduce(data, reduction_op);
+}
+
+// Partially-full warp reduction.
+template <typename T, typename ReductionOp, typename WarpReduce>
+  static __device__ __forceinline__
+  T DeviceReduce(typename WarpReduce::TempStorage &temp_storage,
+                 T &data, 
+                 ReductionOp &reduction_op,
+                 cub::Int2Type<PARTIAL> test_mode,
+                 const int &valid_warp_threads) {
+  return WarpReduce(temp_storage).Reduce(data, reduction_op, valid_warp_threads);
+}
+
 /**
- * Full-tile warp reduction kernel
+ * Warp reduction kernel
  */
-template <int WARPS, int LOGICAL_WARP_THREADS, typename T, typename ReductionOp>
-__global__ void FullWarpReduceKernel(T *d_in, T *d_out, ReductionOp reduction_op,
-                                     clock_t *d_elapsed) {
+template <int NUM_WARPS, int LOGICAL_WARP_THREADS, WarpReduceMode TEST_MODE, 
+          typename T, typename ReductionOp>
+__global__ void WarpReduceKernel(T *d_in, T *d_out, ReductionOp reduction_op,
+                                     clock_t *d_elapsed, int valid_warp_threads) {
   // Cooperative warp-reduce utility type (1 warp)
   typedef cub::WarpReduce<T, LOGICAL_WARP_THREADS> WarpReduce;
 
   // Allocate temp storage in shared memory
-  __shared__ typename WarpReduce::TempStorage temp_storage[WARPS];
+  __shared__ typename WarpReduce::TempStorage temp_storage[NUM_WARPS];
 
   // Per-thread tile data
   T input = d_in[threadIdx.x];
@@ -51,9 +90,14 @@ __global__ void FullWarpReduceKernel(T *d_in, T *d_out, ReductionOp reduction_op
   // Test warp reduce
   int warp_id = threadIdx.x / LOGICAL_WARP_THREADS;
 
-  //T output = DeviceTest<T, ReductionOp, WarpReduce>::Reduce(
-  //  temp_storage[warp_id], input, reduction_op);
-  T output = WarpReduce(temp_storage[warp_id]).Reduce(input, reduction_op);
+  /// The key function. 
+  /// You can call them directly like the two lines below:
+  //T output = WarpReduce(temp_storage[warp_id]).Reduce(input, reduction_op); //Full
+  //T output = WarpReduce(temp_storage[warp_id]).Reduce(input, reduction_op, valid_warp_threads); //Partial
+  /// Or you can use a template to switch them, like this way:
+  T output = DeviceReduce<T, ReductionOp, WarpReduce>(
+    temp_storage[warp_id], input, reduction_op, 
+    cub::Int2Type<TEST_MODE>(), valid_warp_threads);
 
   // Record elapsed clocks
   __threadfence_block();      // workaround to prevent clock hoisting
@@ -67,59 +111,19 @@ __global__ void FullWarpReduceKernel(T *d_in, T *d_out, ReductionOp reduction_op
     output :
     input;
 }
-
-/**
- * Partially-full warp reduction kernel
- */
-template <int WARPS, int LOGICAL_WARP_THREADS, typename T, typename ReductionOp>
-__global__ void PartialWarpReduceKernel(T *d_in, T *d_out, ReductionOp reduction_op,
-                                        clock_t *d_elapsed, int valid_warp_threads) {
-  // Cooperative warp-reduce utility type
-  typedef cub::WarpReduce<T, LOGICAL_WARP_THREADS> WarpReduce;
-
-  // Allocate temp storage in shared memory
-  __shared__ typename WarpReduce::TempStorage temp_storage[WARPS];
-
-  // Per-thread tile data
-  T input = d_in[threadIdx.x];
-
-  // Record elapsed clocks
-  __threadfence_block();      // workaround to prevent clock hoisting
-  clock_t start = clock();
-  __threadfence_block();      // workaround to prevent clock hoisting
-
-  // Test partial-warp reduce
-  int warp_id = threadIdx.x / LOGICAL_WARP_THREADS;
-  //T output = DeviceTest<T, ReductionOp, WarpReduce>::Reduce(
-  //  temp_storage[warp_id], input, reduction_op, valid_warp_threads);
-  T output = WarpReduce(temp_storage[warp_id]).Reduce(input, reduction_op, valid_warp_threads);
-
-  // Record elapsed clocks
-  __threadfence_block();      // workaround to prevent clock hoisting
-  clock_t stop = clock();
-  __threadfence_block();      // workaround to prevent clock hoisting
-
-  *d_elapsed = stop - start;
-
-  // Store aggregate
-  d_out[threadIdx.x] = (threadIdx.x % LOGICAL_WARP_THREADS == 0) ?
-    output :
-    input;
-}
-
 
 /**
  * Test warp reduction
  */
-template <int WARPS, int LOGICAL_WARP_THREADS,
+template <int NUM_WARPS, int LOGICAL_WARP_THREADS,
           typename T, typename ReductionOp>
 void TestReduce(ReductionOp reduction_op,
                 int valid_warp_threads = LOGICAL_WARP_THREADS) {
 
-  const int BLOCK_THREADS = LOGICAL_WARP_THREADS * WARPS;
+  const int BLOCK_THREADS = LOGICAL_WARP_THREADS * NUM_WARPS;
 
   printf("%d warps, %d warp threads, %d valid lanes, %s (%d bytes) elements:\n",
-    WARPS,
+    NUM_WARPS,
     LOGICAL_WARP_THREADS,
     valid_warp_threads,
     typeid(T).name(),
@@ -152,15 +156,16 @@ void TestReduce(ReductionOp reduction_op,
   // Run kernel
   if (valid_warp_threads == LOGICAL_WARP_THREADS) {
     // Run full-warp kernel
-    FullWarpReduceKernel<WARPS, LOGICAL_WARP_THREADS> << <1, BLOCK_THREADS >> > (
+    WarpReduceKernel<NUM_WARPS, LOGICAL_WARP_THREADS, FULL> << <1, BLOCK_THREADS >> > (
       d_in,
       d_out,
       reduction_op,
-      d_elapsed);
+      d_elapsed,
+      valid_warp_threads);
   }
   else {
     // Run partial-warp kernel
-    PartialWarpReduceKernel<WARPS, LOGICAL_WARP_THREADS> << <1, BLOCK_THREADS >> > (
+    WarpReduceKernel<NUM_WARPS, LOGICAL_WARP_THREADS, PARTIAL> << <1, BLOCK_THREADS >> > (
       d_in,
       d_out,
       reduction_op,
@@ -185,7 +190,7 @@ void TestReduce(ReductionOp reduction_op,
 
   // Merge the result of each warp.
   T h_out4cub = 0;
-  for (int i = 0; i < WARPS; i++) {
+  for (int i = 0; i < NUM_WARPS; i++) {
     h_out4cub += h_out4cub_temp_arr[i*LOGICAL_WARP_THREADS + 0];
   }
   std::cout << "Result: (h_out vs h_out4cub) == (" << h_out << " vs " \
@@ -193,7 +198,7 @@ void TestReduce(ReductionOp reduction_op,
 
   // Cleanup
   if (h_in) delete[] h_in;
-  //if (h_out4cub_temp_arr) delete[] h_out4cub_temp_arr;
+  if (h_out4cub_temp_arr) delete[] h_out4cub_temp_arr;
   if (d_in) CubDebugExit(g_allocator.DeviceFree(d_in));
   if (d_out) CubDebugExit(g_allocator.DeviceFree(d_out));
   if (d_elapsed) CubDebugExit(g_allocator.DeviceFree(d_elapsed));
@@ -206,18 +211,20 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  // normal type:
+  // normal:
   //   (unsigned)char / (unsigned) short / (unsigned) int / (unsigned) long long 
   //   float / double
   //
-  // vector type:
+  // vector:
   //   uchar1 
   //   uchar2 / ushort2 / uint2 / ulonglong2
   //   uchar4 / ushort4 / uint4 / ulonglong4
   // 
   // functor:
-  // Sum / Max / Min / ArgMax / CastOp / ...
-  TestReduce<1, 32, int>(cub::Sum());
+  //   Sum / Max / Min / ArgMax / CastOp / ...
+  TestReduce<1, 32, int>(cub::Sum()); 
+  TestReduce<1, 32, int>(cub::Max()); // It just takes the largest number in the input array.
+  TestReduce<1, 32, int>(cub::Sum(), 30); // Partial, Only calculate the first 30 Numbers.
   TestReduce<2, 16, int>(cub::Sum());
   TestReduce<1, 32, double>(cub::Sum());
 
