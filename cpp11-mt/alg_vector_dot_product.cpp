4486c5abdb1473004dee81f79202c50a481be727
Jianming Chen <398342899@qq.com>
2019-02-11 15:39:35 +0800

    [cpp11-mt] Update alg_vector_dot_product.

--- cpp11-mt/alg_vector_dot_product.cpp
+++ cpp11-mt/alg_vector_dot_product.cpp
@@ -1,19 +1,27 @@
 /*!
 * \brief Vector dot product: h_result = SUM(A * B).
-*        Record the basic usage of std::tread.
+*        V1: Record the basic usage of std::thread.
+*        V2: Record the basic usage of std::sync.
 */
 
 #include<iostream>
 #include<thread>
+#include<future>
 
-void DotProduct(float *src0, float *src1, int len, float *dst) {
+void DotProductSerial(float *src0, float *src1, int len, float *dst) {
   *dst = 0;
   for (int i = 0; i < len; i++) {
     *dst += src0[i] * src1[i];
   }
 }
 
-void DotProductMT(float *src0, float *src1, int len, int num_threads, float *dst) {
+// MT Version 1.
+void DotProductMTv1(float *src0, float *src1, int len, int num_threads, float *dst) {
+
+  auto DotProduct = [](float *src0, float *src1, int len, float *dst) {
+    DotProductSerial(src0, src1, len, dst);
+  };
+
   int sub_len = len / num_threads;
 
   float *dst_arr = new float[num_threads];
@@ -47,8 +55,42 @@ void DotProductMT(float *src0, float *src1, int len, int num_threads, float *dst
   delete[]threads;
 }
 
+// MT Version 2.
+void DotProductMTv2(float *src0, float *src1, int len, int num_threads, float *dst) {
+  
+  auto DotProduct = [](float *src0, float *src1, int len) {
+    float dst = 0;
+    DotProductSerial(src0, src1, len, &dst);
+    return dst;
+  };
+
+  int sub_len = len / num_threads;
+
+  std::future<float> *dst_arr = new std::future<float>[num_threads];
+  memset(dst, 0, sizeof(float) * num_threads);
+
+  // Process.
+  for (int i = 0; i < num_threads; i++) {
+    dst_arr[i] = std::async(DotProduct, src0 + i * sub_len, src1 + i * sub_len, sub_len);
+  }
+
+  // Merge.
+  *dst = 0;
+  for (int i = 0; i < num_threads; i++) {
+    *dst += dst_arr[i].get();
+  }
+
+  // Deal with the rest
+  int rest_index = len - num_threads * sub_len;
+  if (rest_index) {
+    *dst += DotProduct(src0 + len - rest_index, src1 + len - rest_index, rest_index);
+  }
+
+  delete[]dst_arr;
+}
+
 int main() {
-  int len = 10000000;
+  int len = 12345000;
   float *src0 = new float[len];
   float *src1 = new float[len];
 
@@ -58,13 +100,19 @@ int main() {
 
   float dst = 0;
   clock_t time = clock();
-  DotProductMT(src0, src1, len, 5, &dst);
-  std::cout << "<MT> result: " << dst << ", time consume: "  \
+  DotProductMTv1(src0, src1, len, 12, &dst);
+  std::cout << "<MT thread> result: " << dst << ", time consume: "  \
     << clock() - time << std::endl;
 
   dst = 0;
   time = clock();
-  DotProduct(src0, src1, len, &dst);
+  DotProductMTv2(src0, src1, len, 12, &dst);
+  std::cout << "<MT sync> result: " << dst << ", time consume: " \
+    << clock() - time << std::endl;
+
+  dst = 0;
+  time = clock();
+  DotProductSerial(src0, src1, len, &dst);
   std::cout << "<Serial> result: " << dst << ", time consume: " \
     << clock() - time << std::endl;
 
