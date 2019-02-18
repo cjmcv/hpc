a5617fb9a1af6163687a10c03550dc206c99d32e
Jianming Chen <398342899@qq.com>
2019-02-18 16:50:04 +0800

    [opencl] Update.

--- opencl/alg_dot_product.cpp
+++ opencl/alg_dot_product.cpp
@@ -4,9 +4,9 @@
 
 #include "ocl_util.h"
 
-void DotProductHost(const int* src1, const int* src2, int* dst, int num_elements) {
+void DotProductHost(const int* src1, const int* src2, int* dst, size_t num_elements) {
   *dst = 0;
-  for (int i = 0; i < num_elements; i++) {
+  for (size_t i = 0; i < num_elements; i++) {
     (*dst) += src1[i] * src2[i];
   }
 }
@@ -18,7 +18,7 @@ int main(int argc, char **argv) {
   // 1D var for Total # of work items
   size_t global_work_size = local_work_size * 2;
 
-  printf("Global Work Size = %u, Local Work Size = %u, # of Work Groups = %u\n\n",
+  printf("Global Work Size = %zu, Local Work Size = %zu, # of Work Groups = %zu\n\n",
     global_work_size, local_work_size, (global_work_size % local_work_size + global_work_size / local_work_size));
 
   // Allocate and initialize host arrays.
@@ -31,7 +31,11 @@ int main(int argc, char **argv) {
     h_src2[i] = i;
   }
 
-  //Get an OpenCL platform
+  // Load CL source.
+  cjmcv_ocl_util::KernelLoader *loader = new cjmcv_ocl_util::KernelLoader;
+  loader->Load("../../alg_dot_product.cl");
+
+  // Get an OpenCL platform.
   cl_uint num_platforms;
   OCL_CHECK(clGetPlatformIDs(5, NULL, &num_platforms));
   printf("There are ( %d ) platforms that support OpenCL.\n\n", num_platforms);
@@ -39,16 +43,21 @@ int main(int argc, char **argv) {
   cl_platform_id *platforms = (cl_platform_id*)malloc(sizeof(cl_platform_id) * num_platforms);
   OCL_CHECK(clGetPlatformIDs(num_platforms, platforms, NULL));
 
-  for (int i = 0; i < num_platforms; i++) {
+  for (cl_uint i = 0; i < num_platforms; i++) {
     cjmcv_ocl_util::PrintPlatBasicInfo(platforms[i]);
 
-    //Get the devices
+    // Get the devices
     cl_device_id device;
     OCL_CHECK(clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, 1, &device, NULL));
 
-    //Create the context
+    // Create the context
     cl_context context = clCreateContext(0, 1, &device, NULL, NULL, &err_code);
     OCL_CHECK(err_code);
+
+    // Get Kernel.
+    cl_kernel kernel;
+    loader->CreateProgram(context);
+    loader->GetKernel("DotProductDevice", &kernel);
 
     // Allocate the OpenCL buffer memory objects for source and result on the device GMEM
     cl_mem d_src1 = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(cl_int) * global_work_size, NULL, &err_code);
@@ -56,27 +65,6 @@ int main(int argc, char **argv) {
     cl_mem d_src2 = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(cl_int) * global_work_size, NULL, &err_code);
     OCL_CHECK(err_code);
     cl_mem d_dst = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(cl_int), NULL, &err_code);
-    OCL_CHECK(err_code);
-
-    // Read the OpenCL kernel in from source file
-    const char* source_file = "../../alg_dot_product.cl";
-    size_t kernel_length;			// Byte size of kernel code
-                              // Buffer to hold source for compilation
-    char* kernel_source = cjmcv_ocl_util::LoadProgSource(source_file, "", &kernel_length);
-    if (kernel_source == NULL) {
-      printf("LoadProgSource Failed.\n"); return -1;
-    }
-    else {
-      printf("LoadProgSource (%s): Succeed\n", source_file);
-    }
-
-    // Create the program
-    cl_program program = clCreateProgramWithSource(context, 1, (const char **)&kernel_source, &kernel_length, &err_code);
-    OCL_CHECK(err_code);
-    OCL_CHECK(clBuildProgram(program, 0, NULL, NULL, NULL, NULL));
-
-    // Create the kernel
-    cl_kernel kernel = clCreateKernel(program, "DotProductDevice", &err_code);
     OCL_CHECK(err_code);
 
     // Set the Argument values
@@ -98,6 +86,9 @@ int main(int argc, char **argv) {
 
     // Synchronous/blocking read of results, and check accumulated errors
     OCL_CHECK(clEnqueueReadBuffer(command_queue, d_dst, CL_TRUE, 0, sizeof(cl_int), &h_dst4cl, 0, NULL, NULL));
+	
+	// Block until all tasks in command_queue have been completed.
+	clFinish(command_queue);
     //--------------------------------------------------------
 
     // Compute and compare results on host.
@@ -106,16 +97,19 @@ int main(int argc, char **argv) {
 
     // Cleanup
     if (kernel) OCL_CHECK(clReleaseKernel(kernel));
-    if (program) OCL_CHECK(clReleaseProgram(program));
     if (command_queue) OCL_CHECK(clReleaseCommandQueue(command_queue));
     if (context) OCL_CHECK(clReleaseContext(context));
     if (d_src1) OCL_CHECK(clReleaseMemObject(d_src1));
     if (d_src2) OCL_CHECK(clReleaseMemObject(d_src2));
     if (d_dst) OCL_CHECK(clReleaseMemObject(d_dst));
-
-    if (kernel_source) free(kernel_source);
   }
 
+  if (loader) {
+    loader->UnLoad();
+    delete loader;
+  }
   if (h_src1) free(h_src1);
   if (h_src2) free(h_src2);
+
+  return 0;
 }
--- opencl/alg_vector_add.cpp
+++ opencl/alg_vector_add.cpp
@@ -4,7 +4,7 @@
 
 #include "ocl_util.h"
 
-void VectorAddHost(const float* src1, const float* src2, float* h_dst, int num_elements) {
+void VectorAddHost(const float* src1, const float* src2, float* h_dst, size_t num_elements) {
   for (int i = 0; i < num_elements; i++) {
     h_dst[i] = src1[i] + src2[i];
   }
@@ -16,9 +16,9 @@ int main(int argc, char **argv) {
   size_t local_work_size = 256;
   // 1D var for Total # of work items
   size_t global_work_size = local_work_size * 256;
-  int num_elements = global_work_size + 200;
+  size_t num_elements = global_work_size + 200;
 
-  printf("Global Work Size = %u, Local Work Size = %u, # of Work Groups = %u\n\n",
+  printf("Global Work Size = %zu, Local Work Size = %zu, # of Work Groups = %zu\n\n",
     global_work_size, local_work_size, (global_work_size % local_work_size + global_work_size / local_work_size));
 
   // Allocate and initialize host arrays.
@@ -26,10 +26,14 @@ int main(int argc, char **argv) {
   float *h_src2 = (float *)malloc(sizeof(cl_float) * global_work_size);
   float *h_dst = (float *)malloc(sizeof(cl_float) * num_elements);
   float *h_dst4cl = (float *)malloc(sizeof(cl_float) * global_work_size);
-  for (int i = 0; i < global_work_size; i++) {
+  for (size_t i = 0; i < global_work_size; i++) {
     h_src1[i] = i;
     h_src2[i] = i;
   }
+
+  // Load CL source.
+  cjmcv_ocl_util::KernelLoader *loader = new cjmcv_ocl_util::KernelLoader;
+  loader->Load("../../alg_vector_add.cl");
 
   //Get an OpenCL platform
   cl_uint num_platforms;
@@ -39,7 +43,7 @@ int main(int argc, char **argv) {
   cl_platform_id *platforms = (cl_platform_id*)malloc(sizeof(cl_platform_id) * num_platforms);
   OCL_CHECK(clGetPlatformIDs(num_platforms, platforms, NULL));
 
-  for (int i = 0; i < num_platforms; i++) {
+  for (cl_uint i = 0; i < num_platforms; i++) {
     cjmcv_ocl_util::PrintPlatBasicInfo(platforms[i]);
 
     //Get the devices
@@ -50,33 +54,17 @@ int main(int argc, char **argv) {
     cl_context context = clCreateContext(0, 1, &device, NULL, NULL, &err_code);
     OCL_CHECK(err_code);
 
+    // Get Kernel.
+    cl_kernel kernel;
+    loader->CreateProgram(context);
+    loader->GetKernel("VectorAdd", &kernel);
+
     // Allocate the OpenCL buffer memory objects for source and result on the device GMEM
     cl_mem d_src1 = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(cl_float) * global_work_size, NULL, &err_code);
     OCL_CHECK(err_code);
     cl_mem d_src2 = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(cl_float) * global_work_size, NULL, &err_code);
     OCL_CHECK(err_code);
     cl_mem d_dst = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(cl_float) * global_work_size, NULL, &err_code);
-    OCL_CHECK(err_code);
-
-    // Read the OpenCL kernel in from source file
-    const char* source_file = "../../alg_vector_add.cl";
-    size_t kernel_length;			// Byte size of kernel code
-    // Buffer to hold source for compilation
-    char* kernel_source = cjmcv_ocl_util::LoadProgSource(source_file, "", &kernel_length);
-    if (kernel_source == NULL) {
-	    printf("LoadProgSource Failed.\n"); return -1;
-    }
-    else {
-      printf("LoadProgSource (%s): Succeed\n", source_file);
-    }
-
-    // Create the program
-    cl_program program = clCreateProgramWithSource(context, 1, (const char **)&kernel_source, &kernel_length, &err_code);
-    OCL_CHECK(err_code);
-    OCL_CHECK(clBuildProgram(program, 0, NULL, NULL, NULL, NULL));
-
-    // Create the kernel
-    cl_kernel kernel = clCreateKernel(program, "VectorAdd", &err_code);
     OCL_CHECK(err_code);
 
     // Set the Argument values
@@ -87,7 +75,8 @@ int main(int argc, char **argv) {
 
     //--------------------------------------------------------
     // Create a command-queue
-    cl_command_queue command_queue = clCreateCommandQueue(context, device, 0, &err_code);
+    // CL_QUEUE_PROFILING_ENABLE: Opencl operation time can only be calculated when this flag bit is turned on
+    cl_command_queue command_queue = clCreateCommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err_code);
     OCL_CHECK(err_code);
     
     // Asynchronous write of data to GPU device
@@ -95,10 +84,20 @@ int main(int argc, char **argv) {
     OCL_CHECK(clEnqueueWriteBuffer(command_queue, d_src2, CL_FALSE, 0, sizeof(cl_float) * global_work_size, h_src2, 0, NULL, NULL));
 
     // Launch kernel
-    OCL_CHECK(clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_work_size, &local_work_size, 0, NULL, NULL));
+    cl_event ev;
+    OCL_CHECK(clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_work_size, &local_work_size, 0, NULL, &ev));
 
     // Synchronous/blocking read of results, and check accumulated errors
     OCL_CHECK(clEnqueueReadBuffer(command_queue, d_dst, CL_TRUE, 0, sizeof(cl_float) * global_work_size, h_dst4cl, 0, NULL, NULL));
+    
+    // Block until all tasks in command_queue have been completed.
+    clFinish(command_queue);
+  
+    // Gets the running time of the kernel function.
+    cl_ulong start_time = 0, end_time = 0;
+    OCL_CHECK(clGetEventProfilingInfo(ev, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start_time, NULL));
+    OCL_CHECK(clGetEventProfilingInfo(ev, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end_time, NULL));
+    printf("kernel exec time: %f ms\n", (end_time - start_time)*1e-6);
     //--------------------------------------------------------
 
     // Compute and compare results on host.
@@ -114,17 +113,17 @@ int main(int argc, char **argv) {
 
     // Cleanup
     if (kernel) OCL_CHECK(clReleaseKernel(kernel));
-    if (program) OCL_CHECK(clReleaseProgram(program));
     if (command_queue) OCL_CHECK(clReleaseCommandQueue(command_queue));
     if (context) OCL_CHECK(clReleaseContext(context));
     if (d_src1) OCL_CHECK(clReleaseMemObject(d_src1));
     if (d_src2) OCL_CHECK(clReleaseMemObject(d_src2));
     if (d_dst) OCL_CHECK(clReleaseMemObject(d_dst));
-
-    if (kernel_source) free(kernel_source);
   }
 
-
+  if (loader) {
+    loader->UnLoad();
+    delete loader;
+  }
   if (h_src1) free(h_src1);
   if (h_src2) free(h_src2);
   if (h_dst) free(h_dst);
