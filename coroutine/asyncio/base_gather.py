c3f488676fa9116f4b5e97a0f5856ef233f7b419
Jianming Chen <398342899@qq.com>
2019-04-15 19:46:10 +0800

    [co] base_gather.

---
+++ coroutine/asyncio/base_gather.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+brief: 
+	Use gather to execute tasks in parallel.
+"""
+
+import asyncio
+
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number+1):
+        print("Task %s: Compute factorial(%s)..." % (name, i))
+        await asyncio.sleep(1)
+        f *= i
+    print("Task %s: factorial(%s) = %s" % (name, number, f))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.gather(
+    factorial("A", 2),
+    factorial("B", 3),
+    factorial("C", 4),
+    factorial("D", 5),
+))
+loop.close()
